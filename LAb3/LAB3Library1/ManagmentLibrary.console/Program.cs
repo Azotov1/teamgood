62f52de8f33c388c50c09687713a00a22bc31311
Azotov1 <voroshulov144@gmail.com>
2023-12-25 18:44:17 +0200

    LAB3 FULL

---
+++ LAb3/LAB3Library1/ManagmentLibrary.console/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+ TaskManagementLibrary TaskLibrary = new();
+TaskLibrary.AddProject("Some project name");
+TaskLibrary.AddProject("hello world");
+TaskLibrary.AddTask("Some title", "Some description", "Some project name");
+TaskLibrary.AddToFavorites(1);
+foreach (var task in TaskLibrary.GetSortedFavorites())
+Console.WriteLine(task);
